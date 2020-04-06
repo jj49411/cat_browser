ff05346c3717abb70cde24172eccea3f78c27fd9
Julie <j49411@gmail.com>
2020-04-06 07:07:24 +0100

    chore: set up mongodb connection

--- server.js
+++ server.js
@@ -2,4 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
+const port = process.env.PORT || 5000
 
+const uri = require('./config/keys').mongoURI
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
+app.listen(port, () => {
+  console.log(`Server is up on port: ${port}`)
+})
