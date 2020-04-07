943b22433a8186e7f5d2ef7c081b12bb4bf65ce1
Julie <j49411@gmail.com>
2020-04-07 19:23:58 +0100

    feat: render cat pictures from Cat API

--- client/src/components/Pictures/Pictures.js
+++ client/src/components/Pictures/Pictures.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Pictures extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      catsData: [],
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    
+    axios.get('https://api.thecatapi.com/v1/images/search', {
+      headers: {'x-api-key': process.env.API_KEY}, 
+      params: {limit: 50}
+    })
+      .then(response => {
+        this.setState({
+          isLoading: true,
+          catsData: response.data
+      })
+    })
+  }
+
+
   render() {
+    const { catsData, isLoading } = this.state
+
     return(
-      <div className='pictures'></div>
+      <div className='pictures'>
+        {catsData.map(cat => (
+          <img src={cat.url} width='300' height='300'/>
+        ))}
+      </div>
+
     )
   }
 }
--- client/src/components/Pictures/Pictures.test.js
+++ client/src/components/Pictures/Pictures.test.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Pictures from './Pictures'
+import axios from 'axios'
 
+jest.mock('axios')
 
 describe('Pictures', () => {
 
   let wrapper
-
+  const dataMock = { id: "testID", url: "testURL"}
+  axios.get.mockImplementationOnce(() => Promise.resolve({ data: dataMock }));
+  
   beforeEach(() => {
     wrapper = shallow(<Pictures />)
   })
@@ -14,5 +18,11 @@ describe('Pictures', () => {
   it('should have a div', () => {
     expect(wrapper.find('.pictures')).toBeDefined()
     expect(wrapper.find('.pictures')).toHaveLength(1)
+  })
+
+  it('should fetch image data from API', async () => {
+    expect(axios.get).toHaveBeenCalled();
+    expect(axios.get).toHaveBeenCalledWith("https://api.thecatapi.com/v1/images/search", 
+    {"headers": {"x-api-key": process.env.API_KEY}, "params": {"limit": 50}})
   })
 })
