f0a079996269a69002106057b0b30613f6a1c6b7
Julie <j49411@gmail.com>
2020-04-25 17:01:21 +0100

    chore: deploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParse = require('body-parser')
+const path = require('path')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
@@ -18,6 +19,15 @@ mongoose
 
 app.use(bodyParse.json())
 app.use('/favourites', favouritesRouter)
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`)
